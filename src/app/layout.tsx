1a7f1cf03ceb30cbf8d1b8f58bc732865103398b
Chris <c.k.onderhoudsbedrijf@gmail.com>
2025-06-13 23:14:49 +0200

    Update favicon to use blob.png instead of default Next.js favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,15 +8,22 @@ export const metadata: Metadata = {
   title: "WOMP - The Ultimate Blob Coin",
   description: "Join the WOMP revolution! The cutest and most powerful blob coin in the crypto universe. Womp womp womp!",
   keywords: ["WOMP", "memecoin", "cryptocurrency", "blob", "DeFi", "token"],
+  icons: {
+    icon: "/images/blob.png",
+    shortcut: "/images/blob.png",
+    apple: "/images/blob.png",
+  },
   openGraph: {
     title: "WOMP - The Ultimate Blob Coin",
     description: "Join the WOMP revolution! The cutest and most powerful blob coin in the crypto universe.",
     type: "website",
+    images: ["/images/blob.png"],
   },
   twitter: {
     card: "summary_large_image",
     title: "WOMP - The Ultimate Blob Coin",
     description: "Join the WOMP revolution! The cutest and most powerful blob coin in the crypto universe.",
+    images: ["/images/blob.png"],
   },
 };
 
